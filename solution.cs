d3c164b0ad978deb7fd557a14fccf7c1c361d0f4
Kooreis <kooreis.official@gmail.com>
2023-05-22 03:16:37 -0500

    This update adds the insert method to the MinHeap class. This method adds a new number to the heap and then reorders the heap to maintain the min heap property.

--- solution.cs
+++ solution.cs
@@ -1,5 +1,16 @@
-class MinHeap {
-    constructor() {
-        this.heap = [null];
+insert(num) {
+        this.heap.push(num);
+        if (this.heap.length > 2) {
+            let idx = this.heap.length - 1;
+            while (this.heap[idx] < this.heap[Math.floor(idx/2)]) {
+                if (idx >= 1) {
+                    [this.heap[Math.floor(idx/2)], this.heap[idx]] = [this.heap[idx], this.heap[Math.floor(idx/2)]];
+                    if (Math.floor(idx/2) > 1) {
+                        idx = Math.floor(idx/2);
+                    } else {
+                        break;
+                    };
+                };
+            };
+        };
     }
-}
